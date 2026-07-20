637d648663c2815f6f93cd38c5137c2f35a62fec
Sumaya <sheikhsumaya622@gmail.com>
2026-07-20 21:41:49 +0600

    server and client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import LikeButton from "./ui/LikeButton";
 
 export default function Home() {
+  console.log("Server Root")
   return (
     <div>
       Hello Sumu!
       <br />
       Go to blog page : <Link href={"/blogs"}> Blogs</Link>
+      <br />
+      <LikeButton></LikeButton>
     </div>
   );
 }
---
+++ app/ui/LikeButton.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const LikeButton = () => {
+  return (
+    <button onClick={() => console.log("Clicked")}>
+        Click
+    </button>
+  )
+}
+
+export default LikeButton
