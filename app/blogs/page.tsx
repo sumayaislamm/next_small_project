aa5860ffdaaf82ed2777d10673b99546c4359dc9
Sumaya <sheikhsumaya622@gmail.com>
2026-07-20 22:38:14 +0600

    fetching the url

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,10 +1,13 @@
-
-
-const page = () => {
+const page = async () => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const postData = await posts.json();
+  // console.log(postData)
   return (
-    <div>Blog page
+    <div>
+      Blog page
+      <h1>Data Length: {postData.length}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
